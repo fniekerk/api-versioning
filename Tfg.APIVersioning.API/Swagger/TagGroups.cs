708d5a4892eb62f161fd6d4352cd2337bd7e2efc
FrederickN <FrederickN@tfg.co.za>
2022-11-22 17:07:57 +0200

    Latest updates.

--- Tfg.APIVersioning.API/Swagger/TagGroups.cs
+++ Tfg.APIVersioning.API/Swagger/TagGroups.cs
@@ -6,6 +6,7 @@ using Microsoft.OpenApi.Extensions;
 using Microsoft.OpenApi.Interfaces;
 using Microsoft.OpenApi.Models;
 using Swashbuckle.AspNetCore.SwaggerGen;
+using System.Diagnostics;
 
 namespace Tfg.APIVersioning.API.Swagger
 {
@@ -13,47 +14,75 @@ namespace Tfg.APIVersioning.API.Swagger
     {
         public void Apply(OpenApiDocument swaggerDoc, DocumentFilterContext context)
         {
-            var tagList = new OpenApiArray
+            var recipeTagList = CreateTagArr(new List<string> { "recipe_model" });
+            var genTagList = CreateTagArr(new List<string> { "Recipe" });
+            var xTagGroups = CreateCustomTag(new Dictionary<string, OpenApiArray>
             {
-                new OpenApiString("recipe_model")
-            };
+                { "General", genTagList },
+                { "Models", recipeTagList }
+            });
 
-            var genTagList = new OpenApiArray
-            {
-                new OpenApiString("Recipe")
-            };
+            swaggerDoc.Extensions = CreateExtension(new Dictionary<string, OpenApiArray> { { "x-tagGroups",  xTagGroups } });
 
-            swaggerDoc.Extensions = new Dictionary<string, IOpenApiExtension>
-            {
-                {
-                    "x-tagGroups",
-                    new OpenApiArray
+            swaggerDoc.Tags.Add(CreateGlobalTag("recipe_model",
+                   "<SchemaDefinition schemaRef=\"#/components/schemas/Recipe\" />",
+                    new Dictionary<string, IOpenApiExtension>
                     {
-                        new OpenApiObject
-                        {
-                            ["name"] = new OpenApiString("General"),
-                            ["tags"] = genTagList
-                        },
-                        new OpenApiObject
-                        {
-                            ["name"] = new OpenApiString("Models"),
-                            ["tags"] = tagList
-                        }
-                    }
-                }
+                        { "x-displayName", new OpenApiString("The Recipe Model") }
+                    })
+            );
+        }
+
+        private OpenApiTag CreateGlobalTag(string tagName, string tagDescription, Dictionary<string, IOpenApiExtension> tagExt)
+        {
+            var globalTag = new OpenApiTag
+            {
+                Name = tagName,
+                Description = tagDescription,
+                Extensions = tagExt
             };
 
-            swaggerDoc.Tags.Add(
-                new OpenApiTag
+            return globalTag;
+        }
+
+        private static OpenApiArray CreateTagArr(List<string> tagList)
+        {
+            var tagArr = new OpenApiArray();
+            foreach(var tag in tagList)
+            {
+                tagArr.Add(new OpenApiString(tag));
+            }
+
+            return tagArr;
+        }
+
+
+        private static OpenApiArray CreateCustomTag (Dictionary<string, OpenApiArray> tags)
+        {
+            OpenApiArray customTag = new OpenApiArray();
+
+            foreach(var tag in tags)
+            {
+                customTag.Add(new OpenApiObject
                 {
-                    Name = "recipe_model",
-                    Description = "<SchemaDefinition schemaRef=\"#/components/schemas/Recipe\" />",
-                    Extensions = new Dictionary<string, IOpenApiExtension>
-                    { 
-                        { "x-displayName", new OpenApiString("The Recipe Model") }
-                    }
-                }
-            );
+                    ["name"] = new OpenApiString(tag.Key),
+                    ["tags"] = tag.Value
+                });
+            }
+
+            return customTag;
+        }
+
+        private static IDictionary<string, IOpenApiExtension> CreateExtension(Dictionary<string, OpenApiArray> customExts)
+        {
+            var customExtCreated = new Dictionary<string, IOpenApiExtension>();
+
+            foreach(var customExt in customExts)
+            {
+                customExtCreated.Add(customExt.Key, customExt.Value);
+            }
+
+            return customExtCreated;
         }
     }
 }
